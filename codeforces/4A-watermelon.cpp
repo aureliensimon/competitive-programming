c74bd6a132fb553231a33d1d41406b75bdc6913e
aureliensimon <aureliensimon29260@gmail.com>
2020-09-04 01:39:42 +0200

    + codeforces/ & projectEuler/

---
+++ codeforces/4A-watermelon.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main () {
+	int i;
+	cin >> i;
+
+	if (i % 2 || i <= 2) {
+		cout << "NO" << endl;
+	} else {
+		cout << "YES" << endl;
+	}
+
+	return 0;
+}
