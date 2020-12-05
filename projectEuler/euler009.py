fdd1b365c79c6bc2abc353ade13ed190bd635293
aureliensimon <aureliensimon29260@gmail.com>
2020-12-05 09:14:36 +0100

    add euler n°9

---
+++ projectEuler/euler009.py
@@ -0,0 +1,23 @@
+import sys
+
+'''
+A Pythagorean triplet is a set of three natural numbers, a < b < c, for which,
+
+a² + b² = c²
+For example, 3² + 4² = 9 + 16 = 25 = 5².
+
+There exists exactly one Pythagorean triplet for which a + b + c = 1000.
+Find the product abc.
+'''
+
+def uniqueTriplet (target):
+    val = -1
+    for a in range(1, int(target / 3)):
+        b = int((target * target - 2 * target * a)/(2 * target - 2 * a))
+        c = target - a - b
+        if ((a**2 + b**2) == c**2) and (a*b*c > val):
+            val = a * b *c
+    return val
+
+n = 1000
+print(uniqueTriplet(n))
